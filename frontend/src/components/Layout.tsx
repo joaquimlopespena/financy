2061c755474eb71345167977dd1dcf4458e0e88d
JoaquimLP <joaquimlp2013@gmail.com>
2026-03-28 13:41:23 -0300

    Fix

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "./ui/sonner";
 
 export function Layout() {
     return (
-        <div className="flex min-h-screen flex-col bg-gray-100">
+        <div className="flex min-h-screen flex-col bg-gray-100 pb-10">
             <Header />
             <main className="mx-auto mt-8 w-full max-w-[calc(100%-2rem)] flex-1 items-center gap-3 sm:px-6 lg:px-10">
                 <Outlet />
